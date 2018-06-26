3783eb245fd0d1a5b5d7b38f1247726ce231a4b3
ancaciascaiu <anca.ciascaiu@gmail.com>
2018-06-26 16:40:24 -0500

    .times actually counts each iteration in |i|

--- waypoints/blowing_bubbles1.rb
+++ waypoints/blowing_bubbles1.rb
@@ -1,11 +1,12 @@
 array = ['e', 'a', 'c', 'b', 'd']
 times_bubbled = 0
 
-array.length.times do |letter|
+array.length.times do |i|
   left_index  = 0
   right_index = 1
 
-  (array.length - 1).times do |letter|
+  (array.length - 1).times do |i|
+    p letter
     left  = array[left_index]
     right = array[right_index]
     if right < left
