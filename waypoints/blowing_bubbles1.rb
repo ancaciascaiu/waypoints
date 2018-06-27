04a4894000f0ec3a29c429eaee767c510254f537
ancaciascaiu <anca.ciascaiu@gmail.com>
2018-06-27 11:21:31 -0500

    inline the loop/block

--- waypoints/blowing_bubbles1.rb
+++ waypoints/blowing_bubbles1.rb
@@ -1,9 +1,7 @@
 array = ['e', 'a', 'c', 'b', 'd']
 
 array.length.times do |i|
-  (array.length - 1).times do |i|
-    array[i], array[i + 1] = array[i + 1], array[i] if array[i + 1] < array[i]
-  end
+  (array.length - 1).times { |i| array[i], array[i + 1] = array[i + 1], array[i] if array[i + 1] < array[i] }
 end
 
 p array # => ["a", "b", "c", "d", "e"]
