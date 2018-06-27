25beaa6c6f466a4a0a4a08cbabce1fb4990523a8
ancaciascaiu <anca.ciascaiu@gmail.com>
2018-06-27 11:17:24 -0500

    write condition inline

--- waypoints/blowing_bubbles1.rb
+++ waypoints/blowing_bubbles1.rb
@@ -2,9 +2,7 @@ array = ['e', 'a', 'c', 'b', 'd']
 
 array.length.times do |i|
   (array.length - 1).times do |i|
-    if array[i + 1] < array[i]
-      array[i], array[i + 1] = array[i + 1], array[i]
-    end
+    array[i], array[i + 1] = array[i + 1], array[i] if array[i + 1] < array[i]
   end
 end
 
