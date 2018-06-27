ba9212563dfc144ecb33b5e32104a059694c3da8
ancaciascaiu <anca.ciascaiu@gmail.com>
2018-06-27 11:12:58 -0500

    remove times_bubbled because it's not really used

--- waypoints/blowing_bubbles1.rb
+++ waypoints/blowing_bubbles1.rb
@@ -1,5 +1,4 @@
 array = ['e', 'a', 'c', 'b', 'd']
-times_bubbled = 0
 
 array.length.times do |i|
   (array.length - 1).times do |i|
@@ -7,8 +6,6 @@ array.length.times do |i|
       array[i], array[i + 1] = array[i + 1], array[i]
     end
   end
-
-  times_bubbled += 1
 end
 
 p array # => ["a", "b", "c", "d", "e"]
