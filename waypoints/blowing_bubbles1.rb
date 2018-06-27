8da2df83a3076ff009c64f0885fbab83f4c06bce
ancaciascaiu <anca.ciascaiu@gmail.com>
2018-06-27 10:45:29 -0500

    remove assignment to left_index

--- waypoints/blowing_bubbles1.rb
+++ waypoints/blowing_bubbles1.rb
@@ -2,18 +2,15 @@ array = ['e', 'a', 'c', 'b', 'd']
 times_bubbled = 0
 
 array.length.times do |i|
-  left_index  = 0
   right_index = 1
 
   (array.length - 1).times do |i|
-    p letter
-    left  = array[left_index]
+    left  = array[i]
     right = array[right_index]
     if right < left
-      array[left_index]  = right
+      array[i] = right
       array[right_index] = left
     end
-    left_index  += 1
     right_index += 1
   end
 
