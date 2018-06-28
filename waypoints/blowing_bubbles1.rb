19d10788fa45cf562861b9d288ba1bbb60cde14b
ancaciascaiu <anca.ciascaiu@gmail.com>
2018-06-27 22:29:55 -0500

    reintroduce inner while loop

--- waypoints/blowing_bubbles1.rb
+++ waypoints/blowing_bubbles1.rb
@@ -3,9 +3,9 @@ array = ['e', 'a', 'c', 'b', 'd']
 array.length.times do |i|
   left_index  = 0
   right_index = 1
-  (array.length - 1).times do |i|
-    left  = array[left_index] # e
-    right = array[right_index] # a
+  while right_index < array.length do
+    left  = array[left_index]
+    right = array[right_index]
     if right < left
       left_letter = left
       array[left_index] = right
