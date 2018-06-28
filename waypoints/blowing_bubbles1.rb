776f2e462cdda268aaf484f3bed8c0c933b054c3
ancaciascaiu <anca.ciascaiu@gmail.com>
2018-06-27 22:00:01 -0500

    un-inline the if statement

--- waypoints/blowing_bubbles1.rb
+++ waypoints/blowing_bubbles1.rb
@@ -2,7 +2,9 @@ array = ['e', 'a', 'c', 'b', 'd']
 
 array.length.times do |i|
   (array.length - 1).times do |i|
-    array[i], array[i + 1] = array[i + 1], array[i] if array[i + 1] < array[i]
+    if array[i + 1] < array[i]
+      array[i], array[i + 1] = array[i + 1], array[i]
+    end
   end
 end
 
