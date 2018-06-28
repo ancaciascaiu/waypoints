37fe049b0044433bed9d0e9dd1fd9b63b36a1c35
ancaciascaiu <anca.ciascaiu@gmail.com>
2018-06-27 22:10:50 -0500

    split the assignments

--- waypoints/blowing_bubbles1.rb
+++ waypoints/blowing_bubbles1.rb
@@ -5,10 +5,10 @@ array.length.times do |i|
     left = i
     right = i + 1
     if array[right] < array[left]
-      array[left], array[right] = array[right], array[left]
+      left_letter = array[left]
+      array[left] = array[right]
+      array[right] = left_letter
     end
-    left += 1
-    right += 1
   end
 end
 
