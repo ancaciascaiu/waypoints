0caeb68b6dcfb46e2874078423b5033e8ca2ecaa
ancaciascaiu <anca.ciascaiu@gmail.com>
2018-06-27 22:36:41 -0500

    remove unnecessary left_letter assignment

--- waypoints/blowing_bubbles1.rb
+++ waypoints/blowing_bubbles1.rb
@@ -1,16 +1,15 @@
 array = ['e', 'a', 'c', 'b', 'd']
 
 times_bubbled = 0
-while times_bubbled < array.length do
+while times_bubbled < array.length
   left_index  = 0
   right_index = 1
-  while right_index < array.length do
+  while right_index < array.length
     left  = array[left_index]
     right = array[right_index]
     if right < left
-      left_letter = left
       array[left_index] = right
-      array[right_index] = left_letter
+      array[right_index] = left
     end
     left_index += 1
     right_index += 1
